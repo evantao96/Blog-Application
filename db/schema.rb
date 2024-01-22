801e92445b104aa60b46f8e4d0291b8458c3ad41
Evan Tao <evantao@Evans-MacBook-Pro-4.local>
2024-01-21 20:09:00 -0500

    user auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_22_004222) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_22_010107) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "post_id", null: false
@@ -28,6 +28,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_22_004222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
