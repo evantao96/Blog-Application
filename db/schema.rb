e4538a576598c877921f95d8a8d1db08f2343b03
Evan Tao <evantao@Evans-MacBook-Pro-4.local>
2024-01-22 00:00:39 -0500

    update pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_22_010107) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "post_id", null: false
