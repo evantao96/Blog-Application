d98b4720946127b7c753afd6735bee4f1c6f6d7e
Evan Tao <evantao@Evans-MBP-4.78M555.nycboe.org>
2024-01-24 13:20:46 -0500

    add welcome spec

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -1,38 +1,5 @@
-describe 'user features' do
+describe 'session features' do
   let!(:user) { User.create( email: 'test@test.com', password: 'test123') }
-  
-  describe 'welcome#index' do
-    
-    before :each do
-      visit '/'
-    end
-
-    it 'loads the correct page successfully' do
-      expect(status_code).to eql(200)
-      expect(page).to have_text('Sign in')
-      expect(page).to have_text('See all blog posts')
-    end
-
-    context 'when logged in' do
-
-      before :each do 
-        visit '/users/sign_in'
-        fill_in 'Email', with: 'test@test.com'
-        fill_in 'Password', with: 'test123'
-        click_button 'Log in'
-        visit '/'
-      end
-
-      it 'loads the correct page successfully' do
-        expect(status_code).to eql(200)
-        expect(page).to have_text('Sign out')
-        expect(page).to have_text('Create a new blog post')
-        expect(page).to have_text('See all blog posts')
-      end
-
-    end
-
-  end
 
   describe 'devise/sessions#new' do 
     before :each do 
