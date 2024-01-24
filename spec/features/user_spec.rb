05fbfbd82b83a936fa521b33b8e5b8a843ab5ce9
Evan Tao <evantao@Evans-MacBook-Pro-4.local>
2024-01-23 23:22:32 -0500

    completed session#destroy tests and started registrations#new tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,7 +1,7 @@
-describe 'user features' do
-
-  describe '#index' do
-    let!(:user) { User.create( email: 'test@test.com', password: 'test123') }
+describe 'devise features' do
+  let!(:user) { User.create( email: 'test@test.com', password: 'test123') }
+  
+  describe 'welcome#index' do
     
     before :each do
       visit '/'
@@ -13,22 +13,13 @@ describe 'user features' do
       expect(page).to have_text('See all blog posts')
     end
 
-    it 'does not show Sign out or Create post links if not logged in as user' do
-      expect(page).to_not have_text('Sign out')
-      expect(page).to_not have_text('Create a new blog post')
-    end
-
     context 'when logged in' do
 
-      def log_in_with(email, password)
-        visit '/users/sign_in'
-        fill_in 'Email', with: email
-        fill_in 'Password', with: password
-        click_button 'Log in'
-      end
-
       before :each do 
-        log_in_with 'test@test.com', 'test123'
+        visit '/users/sign_in'
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Password', with: 'test123'
+        click_button 'Log in'
         visit '/'
       end
 
@@ -39,18 +30,82 @@ describe 'user features' do
         expect(page).to have_text('See all blog posts')
       end
 
-      it 'only shows Sign out, Create post and See post links for logged in user' do
-        expect(page).to have_text('Sign out', count: 1)
-        expect(page).to have_text('Create a new blog post', count: 1)
-        expect(page).to have_text('See all blog posts', count: 1)
-      end
-
-      it 'shows You are already signed in message when attempting to sign in again' do 
-        visit '/users/sign_in'
-        expect(page).to have_text('You are already signed in.')
-      end
-
     end
 
   end
+
+  describe 'devise/sessions#new' do 
+    before :each do 
+      visit 'users/sign_in'
+    end
+
+    it 'loads the correct page successfully' do
+      expect(status_code).to eql(200)
+      expect(page).to have_field('Email')
+      expect(page).to have_field('Password')
+    end
+
+    it 'does not already show signed in user' do
+      expect(page).to_not have_text('Sign out')
+      expect(page).to_not have_text('Create a new blog post')
+    end
+
+    context 'when logged in' do
+      before :each do 
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Password', with: 'test123'
+        click_button 'Log in'
+      end
+
+      it 'loads a page successfully if attempting to sign in again' do 
+        visit 'users/sign_in'
+        expect(status_code).to eql(200)
+      end
+
+      it 'displays You are already signed in if attempting to sign in again' do 
+        visit 'users/sign_in'
+        expect(page).to have_text('You are already signed in.')
+      end
+    end
+  end
+
+  describe 'devise/sessions#destroy' do 
+
+    context 'when signing out' do
+      before :each do 
+        visit 'users/sign_in'
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Password', with: 'test123'
+        click_button 'Log in'
+        click_button 'Sign out'
+      end
+
+      it 'loads a page successfully' do 
+        expect(status_code).to eql(200)
+      end
+
+      it 'loads the correct page' do 
+        expect(page).to have_text('Sign in')
+        expect(page).to have_text('See all blog posts')
+      end
+
+      it 'preserves the user' do 
+        expect(User.count).to eql(1)
+      end
+    end
+  end
+
+  describe 'devise/registrations#new' do 
+    before :each do 
+      visit 'users/sign_up'
+    end
+
+    it 'loads the correct page successfully' do
+      expect(status_code).to eql(200)
+      expect(page).to have_field('Email')
+      expect(page).to have_field('Password')
+      expect(page).to have_field('Password confirmation')
+    end
+  end
+
 end
